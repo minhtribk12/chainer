93384b9f383d48fbf3bbd7e232da9ec1379ec46b
Trí Nguyễn <minhtribk@gmail.com>
2017-09-13 14:59:44 +0700

    iadd_mic function

--- chainer/functions/connection/linear.py
+++ chainer/functions/connection/linear.py
@@ -33,10 +33,11 @@ class LinearFunction(function.Function):
                 b_type.ndim == 1,
                 b_type.shape[0] == w_type.shape[0],
             )
-    def iadd_mic(self, a, b, output_mic):
-        m = a.shape[0]
-        n = a.shape[1]
-        a_ = numpy.tile(input,(m,1))
+    def iadd_mic(self, operand1, operand2, output_mic):
+        m = operand1.shape[0]
+        n = operand1.shape[1]
+        c_ = operand1
+        a_ = numpy.tile(operand2,(m,1))
         device_mic = pymic.devices[0]
         library_mic = device_mic.load_library("libdgemm.so")
         stream_mic = device_mic.get_default_stream()
@@ -45,8 +46,12 @@ class LinearFunction(function.Function):
         b_ = numpy.diag(numpy.ones(n)).reshape((n,n))
         offl_a = stream_mic.bind(a_)
         offl_b = stream_mic.bind(b_)
-        offl_c = stream_mic.bind(a)
+        offl_c = stream_mic.bind(c_)
+        with open("./log/log8.txt","a") as file_log: 
+            file_log.write("invoke start \n")
         stream_mic.invoke(library_mic.dgemm_kernel, offl_a, offl_b, offl_c, m, n, n, alpha_mic, beta_mic)
+        with open("./log/log8.txt","a") as file_log: 
+            file_log.write("invoke end \n")
         stream_mic.sync()
         offl_c.update_host()
         stream_mic.sync()
