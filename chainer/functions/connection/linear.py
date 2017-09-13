9669bb25d00e1277ddf44a18594436085faa4f76
Trí Nguyễn <minhtribk@gmail.com>
2017-09-13 13:37:12 +0700

    replace +=

--- chainer/functions/connection/linear.py
+++ chainer/functions/connection/linear.py
@@ -3,6 +3,8 @@ from chainer.utils import type_check
 from time import time
 
 import micpy
+import pymic
+import numpy
 
 def _as_mat(x):
     if x.ndim == 2:
@@ -58,7 +60,28 @@ class LinearFunction(function.Function):
             with open("./log/log7.txt","a") as file_log:
                 file_log.write("+= start \n")
             start = time()
-            y += b 
+            #y += b
+            #Tri - modify 13/9/17
+            #Start
+            m = y.shape[0]
+            n = y.shape[1]
+            c_ = y
+            a_ = numpy.tile(b,(m,1))
+            device_mic = pymic.devices[0]
+            library_mic = device_mic.load_library("libdgemm.so")
+            stream_mic = device_mic.get_default_stream()
+            alpha_mic = 1.0
+            beta_mic = 1.0
+            b_ = numpy.diag(numpy.ones(n)).reshape((n,n))
+            offl_a = stream_mic.bind(a_)
+            offl_b = stream_mic.bind(b_)
+            offl_c = stream_mic.bind(c_)
+            stream_mic.invoke(library_mic.dgemm_kernel, offl_a, offl_b, offl_c, m, n, n, alpha_mic, beta_mic)
+            stream_mic.sync()
+            offl_c.update_host()
+            stream_mic.sync()
+            y = offl_c.array
+            # End 
             end = time() - start
             with open("./log/log7.txt","a") as file_log:
                 file_log.write("+= is end \n")
